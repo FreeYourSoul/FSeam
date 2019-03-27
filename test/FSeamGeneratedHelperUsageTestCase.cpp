9cab0fc87107b1cd777f8a662e263246c59d2056
FyS <ballandFyS@gmail.com>
2019-03-27 01:30:30 +0100

    add empty testing file to prepare the helper generate functions

---
+++ test/FSeamGeneratedHelperUsageTestCase.cpp
@@ -0,0 +1,9 @@
+//
+// Created by FyS on 3/27/19.
+//
+
+//FSeam::DupeReturnMethod::DependencyGettable::checkSimpleReturn(42);
+//FSeam::DupeMethod::DependencyGettable::checkInputVariable([](int i, std::string str){
+//
+//});
+//FSeam::Verify::DependencyGettable::checkSimpleInputVariable(1, "dede");
