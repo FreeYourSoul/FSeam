9133707d44f01b7003266c1a287e2fab7be44154
qballand <quentin.balland@inait.ai>
2020-11-24 14:36:38 +0100

    [Nix] : format nix

--- fseam-recipee.nix
+++ fseam-recipee.nix
@@ -1,23 +1,21 @@
-{ rev ? "577de1da89bf553a804fe8ed47fcc6fa1b46e1b5", stdenv,
-  cmake, catch2, python3, python37Packages }:
+{ rev ? "577de1da89bf553a804fe8ed47fcc6fa1b46e1b5", stdenv, cmake, catch2
+, python3, python37Packages }:
 
 stdenv.mkDerivation rec {
 
-    version = "1.0.0";
-    pname = "fseam";
+  version = "1.0.0";
+  pname = "fseam";
 
-    src = builtins.fetchGit{
-        url = "https://github.com/FreeYourSoul/FSeam.git";
-        rev = "${rev}";
-    };
+  src = builtins.fetchGit {
+    url = "https://github.com/FreeYourSoul/FSeam.git";
+    rev = "${rev}";
+  };
 
-    cmakeFlags = [
-        "-DFSEAM_BUILD_TESTS=OFF"
-    ];
+  cmakeFlags = [ "-DFSEAM_BUILD_TESTS=OFF" ];
 
-    doCheck = false;
+  doCheck = false;
 
-    enableParallelBuilding = false;
+  enableParallelBuilding = false;
 
-    buildInputs = [ cmake catch2 python3 python37Packages.ply ];
+  buildInputs = [ cmake catch2 python3 python37Packages.ply ];
 }
